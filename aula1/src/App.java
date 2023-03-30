206ebaa9c8ee12d750e37f60b3d76dcda0278be7
Gustavo Reis de Matos <86541146+GusRMatos@users.noreply.github.com>
2023-03-30 13:48:00 -0300

    adiciona as funçoes da segunda aula

--- aula1/src/App.java
+++ aula1/src/App.java
@@ -1,6 +1,9 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -37,17 +40,40 @@ public class App {
         var parser = new JsonParse();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        //apresenta os dados tratados no terminal
+        //cria diretorio de figurinhas caso nao exista
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+
+        //inicializa o gerador de figurinhas
+        var geradora = new GeradorDeStikers();
+
+        //faz o tratamento dos dados
         for (Map<String,String> filme : listaDeFilmes) {
+            String urlimagem = filme.get("image");
+            String titulo = filme.get("title");
             var rating = filme.get("imDbRating");
             var tam = Double.parseDouble(rating);
 
+            //personaliza o texto das figurinhas de acordo com o rating
+            String texto;
+            if(tam >= 9){
+                texto = "TOPZEIRA!";
+            }else {
+                texto = "MARROMENOS";
+            }
+
+            //gera as figurinhas e armazena no diretorio especificado
+            InputStream inputStream = new URL(urlimagem).openStream();
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
+            geradora.gerar(inputStream, nomeArquivo, texto);
+
+            //apresentação personalizada dos dados no terminal
             for (int x = 0; x < 100; x++) {
                 System.out.print("-");
             }
-            System.out.println("\n\u001B[1mTitulo: \u001B[m" + filme.get("title"));
-            System.out.println("\u001B[1mPoster: \u001B[1m" + filme.get("image"));
-            System.out.println("\u001B[43mClassificação: " + filme.get("imDbRating") + "\u001B[m");
+            System.out.println("\n\u001B[1mTitulo: \u001B[m" + titulo);
+            System.out.println("\u001B[1mPoster: \u001B[1m" + urlimagem);
+            System.out.println("\u001B[43mClassificação: " + rating + "\u001B[m");
 
             for (int x = 0; x < tam; x++){
                 System.out.print("⭐");
